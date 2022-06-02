666a6cf0f24592c92ab294c0a77d39b5b84cc5fa
ignacio.rodriguez3 <ignacio.rodriguez@balloon-group.com>
2022-06-01 23:04:37 -0300

    unique conversation between same users feature

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -1,16 +1,29 @@
 const router = require("express").Router();
 const Conversation = require("../models/Conversation");
+const axios = require("axios");
 
 //new conv
 
 router.post("/", async (req, res) => {
-  const newConversation = new Conversation({
-    members: [req.body.senderId, req.body.receiverId],
-  });
-
   try {
-    const savedConversation = await newConversation.save();
-    res.status(200).json(savedConversation);
+    const newConversation = new Conversation({
+      members: [req.body.senderId, req.body.receiverId],
+    });
+
+    let conversationExists = await axios.get(
+      "http://localhost:8800/api/conversations/find/" +
+        req.body.senderId +
+        "/" +
+        req.body.receiverId
+    );
+    console.log(conversationExists.data);
+    if (conversationExists !== null) {
+      console.log("hola");
+      const savedConversation = await newConversation.save();
+      res.status(200).json(savedConversation);
+    } else {
+      throw error;
+    }
   } catch (err) {
     res.status(500).json(err);
   }
@@ -36,7 +49,7 @@ router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
     const conversation = await Conversation.findOne({
       members: { $all: [req.params.firstUserId, req.params.secondUserId] },
     });
-    res.status(200).json(conversation)
+    res.status(200).json(conversation);
   } catch (err) {
     res.status(500).json(err);
   }
